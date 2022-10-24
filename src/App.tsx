81a7f758c0b8aa65f362a0599f748d9bf4464b30
Polushin Dima <dima2004polushin2004@gmail.com>
2022-10-24 15:25:20 +0600

    Рендеринг карт

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CardView from "./СardView/cardView";
+import Card from "./lib/Card";
+import CardDesk from "./lib/cardDesk";
+import PokerHand from "./lib/PokerHand";
 
 function App() {
-  return (
-    <div className="App">
+  const [card, setCard] = useState<Card[]>([]);
 
+  const pokerHand = new PokerHand();
+  pokerHand.pokerCards.push(...card)
+
+  const getCard = () => {
+    const cardDesk = new CardDesk();
+    setCard(cardDesk.getCards(5));
+  }
+  const button = <button onClick={getCard}>Раздать карты</button>;
+
+
+  if (card.length === 0) {
+    return (
+      <div>
+        Array is empty !
+        {button}
+      </div>
+    );
+  }
+
+
+
+  return (
+    <div className="App playingCards faceImages">
+      <h1>{pokerHand.getOutcome()}</h1>
+      {button}
+      <CardView rank={card[0].suit} suit={card[0].rank}/>
+      <CardView rank={card[1].suit} suit={card[1].rank}/>
+      <CardView rank={card[2].suit} suit={card[2].rank}/>
+      <CardView rank={card[3].suit} suit={card[3].rank}/>
+      <CardView rank={card[4].suit} suit={card[4].rank}/>
     </div>
   );
+
 }
 
 export default App;
+
